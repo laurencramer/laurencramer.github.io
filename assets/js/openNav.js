719f04245012982f1ba5509f0fb95accbb7459bc
Lauren Cramer <lcramer@uci.edu>
2020-02-09 19:33:09 -0800

    Changed file structure, made pages and images more resize-friendly

---
+++ assets/js/openNav.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+function openNav() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
